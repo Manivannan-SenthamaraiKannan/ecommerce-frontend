c40ce730f55a5003ab2780e8dfde80f3e3dfe9b7
Manivannan-SenthamaraiKannan <sharvamanivannan@gmail.com>
2023-10-10 00:15:50 +0530

    Project Initialization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-center'>Ecommerce-Frontend</div>
+    </div>
+  );
+}
+
+export default App;
